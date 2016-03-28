9df71f9cfa86d15817b398f0d864c37a2fef7ee0
Eunbee Hong <hebee935@gmail.com>
2016-03-28 20:41:00 +0900

    #3 implement input user name

--- app/src/main/java/kr/hs/emirim/beee1004/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/beee1004/monchicken/MainActivity.java
@@ -2,12 +2,23 @@ package kr.hs.emirim.beee1004.monchicken;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mName = (EditText)findViewById(R.id.name);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mName.setText("");  //사용자가 백버튼을 눌렀을 경우에 다시 남아있는 것을 방지하기 위해 공백으로 채움
     }
 }
--- app/src/main/java/kr/hs/emirim/beee1004/monchicken/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/beee1004/monchicken/ResultActivity.java
@@ -3,10 +3,10 @@ package kr.hs.emirim.beee1004.monchicken;
 /**
  * Created by appcreate40 on 2016. 3. 28..
  */
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ResultActivity extends ActionBarActivity{
+public class ResultActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
