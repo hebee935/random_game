2b2f13cd7528a01a3bcd26824ce50811d058a953
Eunbee Hong <hebee935@gmail.com>
2016-04-11 21:01:11 +0900

    #8 Add Splash Code

---
+++ app/src/main/java/kr/hs/emirim/beee1004/monchicken/SplashActivity.java
@@ -0,0 +1,32 @@
+package kr.hs.emirim.beee1004.monchicken;
+
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        },3000);
+    }
+
+}
